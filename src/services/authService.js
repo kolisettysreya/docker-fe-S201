4e65bffcc9d6becf42c1f8c86fd1d2d77231987c
KOLISETTY SREYA SRI <kolisettysreya25@gmail.com>
2025-10-07 12:23:23 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-54-235-31-77.compute-1.amazonaws.com:8081/auth";
+const API_URL = "http://ec2-54-89-189-102.compute-1.amazonaws.com:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
@@ -15,4 +15,5 @@ export const signup = async (username, email, password) => {
 export const logout = () => {
   localStorage.removeItem("token");
 };
+
 
